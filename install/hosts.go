eef2107affe3be3c19590d1cc94728f3d5cf2010
pixiake <guotongxue123@163.com>
2020-03-13 23:50:21 +0800

    add log info

--- install/hosts.go
+++ install/hosts.go
@@ -33,7 +33,7 @@ func DockerInstall(nodes *cluster.AllNodes) {
 	installDockerCmd := "curl https://raw.githubusercontent.com/pixiake/kubeocean/master/scripts/docker-istall.sh | sh"
 	if nodes.Hosts == nil && CheckDocker(nil) == false {
 		log.Infof("Docker being installed...")
-		if output, err := exec.Command("curl", "https://raw.githubusercontent.com/pixiake/kubeocean/master/scripts/docker-istall.sh | sh").CombinedOutput(); err != nil {
+		if output, err := exec.Command("/bin/sh", "-c", "curl https://raw.githubusercontent.com/pixiake/kubeocean/master/scripts/docker-istall.sh | sh").CombinedOutput(); err != nil {
 			log.Fatal("Install Docker Failed:\n")
 			fmt.Println(output)
 		}
@@ -114,7 +114,7 @@ func GetKubeBinary(cfg *cluster.ClusterCfg, nodes *cluster.AllNodes) {
 		}
 		exec.Command("/bin/sh", "-c", "chmod +x /usr/local/bin/kubeadm").Run()
 
-		if err := exec.Command("sudo", getKubeletCmd).Run(); err != nil {
+		if err := exec.Command("/bin/sh", "-c", getKubeletCmd).Run(); err != nil {
 			log.Errorf("Failed to get kubelet: %v", err)
 		}
 		exec.Command("/bin/sh", "-c", "chmod +x /usr/local/bin/kubelet").Run()
