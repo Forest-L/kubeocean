bd3229a62603088786ee05846b788fc3f4fac602
pixiake <guotongxue123@163.com>
2020-03-14 15:18:02 +0800

    update install

--- install/prepare.go
+++ install/prepare.go
@@ -2,6 +2,7 @@ package install
 
 import (
 	"fmt"
+	"github.com/pixiake/kubeocean/util"
 	"github.com/pixiake/kubeocean/util/cluster"
 	log "github.com/sirupsen/logrus"
 	"os/exec"
@@ -11,30 +12,47 @@ func InstallFilesDownload(kubeVersion string) {
 	if kubeVersion == "" {
 		kubeVersion = cluster.DefaultKubeVersion
 	}
+
 	kubeadmUrl := fmt.Sprintf("https://kubernetes-release.pek3b.qingstor.com/release/%s/bin/linux/%s/kubeadm", kubeVersion, cluster.DefaultArch)
 	kubeletUrl := fmt.Sprintf("https://kubernetes-release.pek3b.qingstor.com/release/%s/bin/linux/%s/kubelet", kubeVersion, cluster.DefaultArch)
 	kubectlUrl := fmt.Sprintf("https://kubernetes-release.pek3b.qingstor.com/release/%s/bin/linux/%s/kubectl", kubeVersion, cluster.DefaultArch)
 	kubeCniUrl := fmt.Sprintf("https://containernetworking.pek3b.qingstor.com/plugins/releases/download/%s/cni-plugins-linux-%s-%s.tgz", "v0.8.1", cluster.DefaultArch, "v0.8.1")
 
-	getKubeadmCmd := fmt.Sprintf("curl -o /tmp/kubeocean/kubeadm-%s  %s", kubeVersion, kubeadmUrl)
-	getKubeletCmd := fmt.Sprintf("curl -o /tmp/kubeocean/kubelet-%s  %s", kubeVersion, kubeletUrl)
-	getKubectlCmd := fmt.Sprintf("curl -o /tmp/kubeocean/kubectl-%s  %s", kubeVersion, kubectlUrl)
-	getKubeCniCmd := fmt.Sprintf("curl -o /tmp/kubeocean/cni-plugins-linux-amd64-v0.8.1.tgz  %s", kubeCniUrl)
+	kubeadm := fmt.Sprintf("/tmp/kubeocean/kubeadm-%s", kubeVersion)
+	kubelet := fmt.Sprintf("/tmp/kubeocean/kubelet-%s", kubeVersion)
+	kubectl := fmt.Sprintf("/tmp/kubeocean/kubectl-%s", kubeVersion)
+	kubeCni := fmt.Sprintf("/tmp/kubeocean/cni-plugins-linux-%s-%s.tgz", cluster.DefaultArch, "v0.8.1")
+
+	getKubeadmCmd := fmt.Sprintf("curl -o %s  %s", kubeadm, kubeadmUrl)
+	getKubeletCmd := fmt.Sprintf("curl -o %s  %s", kubelet, kubeletUrl)
+	getKubectlCmd := fmt.Sprintf("curl -o %s  %s", kubectl, kubectlUrl)
+	getKubeCniCmd := fmt.Sprintf("curl -o %s  %s", kubeCni, kubeCniUrl)
 
 	log.Info("Kubeadm being download ...")
-	if err := exec.Command("/bin/sh", "-c", getKubeadmCmd).Run(); err != nil {
-		log.Errorf("Failed to get kubeadm: %v", err)
+	if util.IsExist(kubeadm) == false {
+		if err := exec.Command("/bin/sh", "-c", getKubeadmCmd).Run(); err != nil {
+			log.Errorf("Failed to get kubeadm: %v", err)
+		}
 	}
+
 	log.Info("Kubelet being download ...")
-	if err := exec.Command("/bin/sh", "-c", getKubeletCmd).Run(); err != nil {
-		log.Errorf("Failed to get kubelet: %v", err)
+	if util.IsExist(kubelet) == false {
+		if err := exec.Command("/bin/sh", "-c", getKubeletCmd).Run(); err != nil {
+			log.Errorf("Failed to get kubelet: %v", err)
+		}
 	}
+
 	log.Info("Kubectl being download ...")
-	if err := exec.Command("/bin/sh", "-c", getKubectlCmd).Run(); err != nil {
-		log.Errorf("Failed to get kubectl: %v", err)
+	if util.IsExist(kubectl) == false {
+		if err := exec.Command("/bin/sh", "-c", getKubectlCmd).Run(); err != nil {
+			log.Errorf("Failed to get kubectl: %v", err)
+		}
 	}
+
 	log.Info("KubeCni being download ...")
-	if err := exec.Command("/bin/sh", "-c", getKubeCniCmd).Run(); err != nil {
-		log.Errorf("Failed to get kubecni: %v", err)
+	if util.IsExist(kubeCni) == false {
+		if err := exec.Command("/bin/sh", "-c", getKubeCniCmd).Run(); err != nil {
+			log.Errorf("Failed to get kubecni: %v", err)
+		}
 	}
 }
