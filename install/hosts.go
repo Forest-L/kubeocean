c96025cd0baa9e3d7fafac18dd349436d6993718
pixiake <guotongxue123@163.com>
2020-03-16 23:25:24 +0800

    run command for parallel

--- install/hosts.go
+++ install/hosts.go
@@ -64,24 +64,25 @@ func InstallDocker(nodes *cluster.AllNodes) {
 		}
 	} else {
 		log.Infof("Docker being installed")
-		rs := make(chan string)
+		rs := make(chan ssh.Result)
 		cn := make(chan struct{}, ssh.DefaultCon)
 		hostNum := len(nodes.Hosts)
 		defer close(rs)
 		defer close(cn)
 		wg := &sync.WaitGroup{}
-		go ssh.CheckResults(rs, hostNum, wg, cn)
+		go ssh.PrintResults(rs, hostNum, wg, cn)
 
 		for _, node := range nodes.Hosts {
 			cn <- struct{}{}
 			wg.Add(1)
-			go func(host *cluster.ClusterNodeCfg, rs chan string) {
+			go func(host *cluster.ClusterNodeCfg, rs chan ssh.Result) {
 				if err := host.CmdExec(dockerCheckCmd); err != nil {
 					ssh.CmdExec(host.Node.Address, host.Node.User, host.Node.Port, host.Node.Password, true, "", installDockerCmd)
 				} else {
 					log.Infof("Docker already exists. [%s]", host.Node.InternalAddress)
 				}
-				rs <- node.Node.InternalAddress
+				result := ssh.Result{}
+				rs <- result
 			}(&node, rs)
 			fmt.Println(node.Node.InternalAddress)
 		}
